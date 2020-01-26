4beed76331e07824c2003263004c8b2502b482e3
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2020-01-26 16:15:06 +0100

    chore: remove unused import

--- src/main/java/brave/kafka/interceptor/TracingBuilder.java
+++ src/main/java/brave/kafka/interceptor/TracingBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 The OpenZipkin Authors
+ * Copyright 2018-2020 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -14,7 +14,6 @@
 package brave.kafka.interceptor;
 
 import brave.Tracing;
-import brave.propagation.ThreadLocalCurrentTraceContext;
 import brave.sampler.Sampler;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.slf4j.Logger;
